daaf7ba0a46e7a2034b585d47d38fdabad8944b1
yurikbv <yurikbv@yandex.ru>
2022-04-14 15:58:45 +0300

    login mutation

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
 import FormError from '../components/FormError';
 import { gql, useMutation } from '@apollo/client';
+import {
+  loginMutation,
+  loginMutationVariables,
+} from '../../__generated__/loginMutation';
+import logo from '../images/logo.svg';
 
 interface ILoginForm {
   email: string;
   password: string;
+  result?: string;
 }
 
 const LOGIN_MUTATION = gql`
-  mutation PotatoMutation($email: String!, $password: String!) {
-    login(input: { email: $email, password: $password }) {
+  mutation loginMutation($loginInput: LoginInput!) {
+    login(input: $loginInput) {
       ok
       token
       error
@@ -22,65 +29,95 @@ const Login = () => {
   const {
     register,
     handleSubmit,
-    getValues,
-    formState: { errors, isValid },
+    setError,
+    clearErrors,
+    formState: { errors },
   } = useForm<ILoginForm>();
 
-  const [loginMutation, { loading, error, data }] = useMutation(LOGIN_MUTATION);
+  const onCompleted = (data: loginMutation) => {
+    const {
+      login: { error, ok, token },
+    } = data;
+    if (!ok && error) {
+      return setError('result', { message: error });
+    }
+    console.log(token);
+  };
+
+  const [loginMutation, { loading }] = useMutation<
+    loginMutation,
+    loginMutationVariables
+  >(LOGIN_MUTATION, {
+    onCompleted,
+  });
 
   const onSubmit: SubmitHandler<ILoginForm> = data => {
+    if (loading) return;
     const { email, password } = data;
     loginMutation({
       variables: {
-        email,
-        password,
+        loginInput: {
+          email,
+          password,
+        },
       },
     });
   };
 
+  const clearLoginError = () => clearErrors('result');
+
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-800">
-      <div className="bg-white w-full max-w-lg pt-5 pb-7 rounded-lg text-center">
-        <h3 className="text-2xl text-gray-800">Log In</h3>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="grid gap-3 mt-5 px-5"
-        >
-          <input
-            {...register('email', {
-              required: 'Email is required.',
-            })}
-            name="email"
-            type="email"
-            placeholder="email"
-            className="input"
-          />
+    <div className="h-screen flex items-center flex-col mt-32">
+      <img src={logo} alt="Logo" className="w-60 mb-5" />
+      <form onSubmit={handleSubmit(onSubmit)} className="grid gap-3 mt-5 px-5">
+        <input
+          {...register('email', {
+            required: 'Email is required.',
+          })}
+          name="email"
+          type="email"
+          placeholder="email"
+          className="input"
+          onChange={clearLoginError}
+        />
 
-          {errors?.email?.message && (
-            <FormError errorMessage={errors.email.message} />
-          )}
+        <ErrorMessage
+          errors={errors}
+          name="email"
+          render={({ message }) => <FormError errorMessage={message} />}
+        />
 
-          <input
-            {...register('password', {
-              required: 'Password is required.',
-              minLength: {
-                value: 10,
-                message: 'Password must exceed 9 characters',
-              },
-            })}
-            name="password"
-            type="password"
-            placeholder="password"
-            className="input"
-          />
+        <input
+          {...register('password', {
+            required: 'Password is required.',
+            minLength: {
+              value: 6,
+              message: 'Password must exceed 5 characters',
+            },
+          })}
+          name="password"
+          type="password"
+          placeholder="password"
+          className="input"
+          onChange={clearLoginError}
+        />
 
-          {errors?.password?.message && (
-            <FormError errorMessage={errors.password.message} />
-          )}
+        <ErrorMessage
+          errors={errors}
+          name="password"
+          render={({ message }) => <FormError errorMessage={message} />}
+        />
 
-          <button className="btn__form">log in</button>
-        </form>
-      </div>
+        <button className="btn__form" disabled={loading}>
+          {loading ? 'Loading...' : 'Log In'}
+        </button>
+
+        <ErrorMessage
+          errors={errors}
+          name="result"
+          render={({ message }) => <FormError errorMessage={message} />}
+        />
+      </form>
     </div>
   );
 };
